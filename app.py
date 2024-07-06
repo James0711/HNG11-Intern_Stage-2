a2048ce0a7996227ebad8eb28e7213c6f87a7dad
James0711 <jengreat94@gmail.com>
2024-07-06 04:06:11 +0100

    Update app.py and README.md

--- app.py
+++ app.py
@@ -13,10 +13,7 @@ def get_json_result(req, visitor_ip):
     """
     Function that generates and returns the required json response
     """
-    # Moved the code which previously lived inside `hello_visitor` here, 
-    # to achieve DRY code, because same code is now used in both `hello_visitor` 
-    # and the newly added function: `hello_visitor2`
-    
+     
     visitor_name = req.args.get('visitor_name').replace('"', '').replace("'", '')
 
     # get gealocation info for IP address
@@ -35,20 +32,20 @@ def get_json_result(req, visitor_ip):
     # reatun json
     return jsonify(
         {
-        "client_ip": f"{visitor_ip}", # The IP address of the requester
-        "location": f"{visitor_city}", # The city of the requester
+        "client_ip": f"{visitor_ip}", # The IP address of the request
+        "location": f"{visitor_city}", # The city bearing the request
         "greeting": f"Hello, {visitor_name}!, the temperature "
                     f"is {city_temp} degrees Celcius in {visitor_city}",
         }
     )
 
-
+# Creating a home page 
 @app.route("/")
 def home():
     """Just to have something on the home page"""
     return Markup(
         "<h1>Hello, Welcome!</h1> <br>"
-        "<h3>This is HNG11 Stage One Task by Chimeziri Obioha.</h3> <br>"
+        "<h3>This is HNG11 Stage One Task done by Eche James.</h3> <br>"
         f"<h3>Visit <u><em>{request.base_url}api/hello?visitor_name='YOUR NAME'</em></u> for take-away greeting</h3>"
     )
 
@@ -56,8 +53,7 @@ def home():
 @app.route("/api/hello", methods=['GET'])
 def hello_visitor():
     """
-    THIS IS THE MAIN ANSWER TO THE `HNG11-STAGE-1-BACKEND-TASK`
-    The code in `get_json_result` lived here until I started playing around
+    This is the end point that bears the response to the task
     """
     # Get Visitor's IP Address
     if os.environ.get('APP_IN_PRODUCTION'):
@@ -87,28 +83,22 @@ def hello_visitor2():
     return get_json_result(request, request.remote_addr)
 
 
-# @app.route("/api/test-production/", methods=['GET'])
-# def test_production():
-#     """
-#     Just a quick way to test out the production version when in localhost
-#     """
-#     clever_c_prod_url = "https://chimeziri-obioha-hng11-stage1-task.cleverapps.io/api/hello?visitor_name='Chimeziri Obioha'"
-#     vercel_prod_url = "https://hng-11-stg-1-task-chimeziri-obiohas-projects.vercel.app//api/hello?visitor_name='Chimeziri Obioha'"
+@app.route("/api/test-production/", methods=['GET'])
+def test_production():
+    """
+    Testing the production locally
+    """
+   
+    vercel_prod_url = "https://hng-11-intern-stage-2-eche-ngbede-james-projects.vercel.app//api/hello?visitor_name='Eche James'"
 
-#     try:
-#         rep = requests.get(clever_c_prod_url, timeout=1200)
-#         return jsonify(rep.json())
-#     except ValueError:
-#         print("App seems not to be active on CleverCloud")
-
-#     try:
-#         rep = requests.get(vercel_prod_url, timeout=1200)
-#         return jsonify(rep.json())
-#     except ValueError:
-#         print("App seems not to be active on Vercel")
+    try:
+        rep = requests.get(vercel_prod_url, timeout=1200)
+        return jsonify(rep.json())
+    except ValueError:
+        print("App seems not to be active on Vercel")
     
-#     print("App seems not to be active on both CleverCloud and Vercel")
-#     return redirect(url_for('home'))
+    print("App seems not to be active on both CleverCloud and Vercel")
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
